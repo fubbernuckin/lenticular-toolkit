bf034feca8d83c7848f76638db88b7d26dafddc9
fubbernuckin <fubbernuckin@gmail.com>
2024-11-04 00:29:34 -0600

    added header to pitch sheet. added metric units to pitch sheet. altered code to handle different dimensions and aspect ratios better.

--- lenticular/pitch_sheet.py
+++ lenticular/pitch_sheet.py
@@ -9,15 +9,21 @@ from os import path
 
 class PitchSheet:
     #low_pitch and high_pitch are the lower and upper bounds, and use LPI as units
-    #page_dimensions uses inches as units (potentially reconfigurable to millimeters for ISO 216)
+    #page_dimensions uses inches as units
     #page_dpi is the dots per inch of the printer intended to be used. passed as a tuple for printers with non-square max dpi
-    #page_resolution represents width and height of the dpi of the printer in use #???
-    def __init__(self, low_pitch:float, high_pitch:float, page_dimensions:tuple, page_dpi:tuple):
+    #page_resolution represents width and height of the image produced in pixels
+    def __init__(self, low_pitch:float, high_pitch:float, page_dimensions:tuple, page_dpi:tuple, metric:bool = False):
         self.low_pitch = low_pitch
         self.high_pitch = high_pitch
-        self.page_dimensions = page_dimensions
+        if (metric):
+            conv = 1.0/2.54
+            self.page_dimensions = (page_dimensions[0] * conv,
+                                    page_dimensions[1] * conv)
+        else:
+            self.page_dimensions = page_dimensions
         self.page_dpi = page_dpi
         self.page_resolution = tuple(int(ele1 * ele2) for ele1, ele2 in zip(self.page_dimensions, self.page_dpi)) #???
+        self.metric = metric
     
     #generates one segment of the pitch sheet
     #size_in is a tuple providing the dimensions of the segment in inches
@@ -37,8 +43,8 @@ class PitchSheet:
             seg_draw.line([j, 0, j, pre_seg_res[1]], fill = (0, 0, 0), width = 1)
         #resize by ppl/line_spacing
         correction_res = (int(pre_seg_res[0] * (ppl/line_spacing)), int(size_px[1]))
-        seg = seg.resize(correction_res, resample = Image.BICUBIC)
-        seg = seg.crop((int(correction_res[0] - size_px[0]), 0, size_px[0] - (correction_res[0] - size_px[0]), size_px[1]))
+        seg = seg.resize(correction_res, resample = Image.LINEAR)
+        seg = seg.crop((0, 0, size_px[0], size_px[1]))
         
         return seg
     
@@ -49,28 +55,85 @@ class PitchSheet:
     #size_px is a tuple providing the dimensions in pixels
     #lpi is the number that the function will render and place next to the segment
     #text renders aligned left, vertically centered in the box
-    def _generate_segment_label(self, fnt_path, fnt_size, size_in, size_px, lpi):
+    def _generate_segment_label(self, fnt_path, fnt_size, size_px, lpi):
         fnt_vert = size_px[1]/4.0 + fnt_size/2.0 #vertical offset of text
         fnt = ImageFont.truetype(fnt_path, int(size_px[1] * fnt_size)) #font for pitch segment labels
         lab = Image.new(mode = "RGB", size = (int(size_px[0]), int(size_px[1])), color = (255, 255, 255))
         lab_draw = ImageDraw.Draw(lab)
-        lab_draw.text((0,fnt_vert), "{:0.2f}".format(lpi), font = fnt, fill=(0,0,0))
+        lab_draw.text((0,fnt_vert), "{:0.3f}".format(lpi), font = fnt, fill=(0,0,0))
         return lab
     
     #this function generates the image of the text header at the top of the pitch sheet page
-    def _generate_header_text(self):
-        pass
+    #the header text is aligned top center, and the info text is aligned bottom right
+    #fnt_path is an OS path (as in 'from os import path') to a truetype font to be used
+    #info_fnt_size is a float representing the size of the page info font
+    #head_fnt_size is a float representing the size of the header font relative to the vertical size of the image generated, where 0.0 is infinitely small, and 1.0 fills the image vertically
+    #size_px is a tuple providing the dimensions in pixels
+    def _generate_header_text(self, fnt_path, info_fnt_size, head_fnt_size, size_px):
+        #info to include:
+        #page dimensions
+        #page dpi
+        #page resolution
+        
+        head_fnt = ImageFont.truetype(fnt_path, int(size_px[1] * head_fnt_size)) #font for the main header
+        info_fnt = ImageFont.truetype(fnt_path, int(size_px[1] * info_fnt_size)) #font for the info subheader
+        head = Image.new("RGBA", (int(size_px[0]), int(size_px[1])), color = (255, 255, 255, 0))
+        head_draw = ImageDraw.Draw(head)
+        
+        head_pos = (size_px[0] / 2.0, head_fnt_size * size_px[1])
+        head_draw.text(head_pos, "Pitch Sheet", font = head_fnt, anchor = "ms", fill = (0, 0, 0))
+        info_pos = (size_px[0], size_px[1])
+        unit_indicator = ""
+        adjusted_dimensions:tuple
+        if (self.metric):
+            unit_indicator = "cm"
+            adjusted_dimensions = (round(self.page_dimensions[0] * 2.54, 3), round(self.page_dimensions[1] * 2.54, 3))
+        else:
+            unit_indicator = "in"
+            adjusted_dimensions = (round(self.page_dimensions[0], 3), round(self.page_dimensions[0], 3))
+        info_text = f"Page dimensions ({unit_indicator}): {adjusted_dimensions[0]} x {adjusted_dimensions[1]}\n"\
+            f"DPI: {self.page_dpi[0]} x {self.page_dpi[1]}\n"\
+            f"Resolution: {self.page_resolution[0]} x {self.page_resolution[1]}"
+        head_draw.multiline_text(info_pos, info_text, font = info_fnt, anchor = "rd", align = "right", fill = (0, 0, 0))
+        return head
     
     #this function generates the image for the printable pitch sheet and saves it to the specified directory
     #segments represents how many rows of pitch tests will print on the page
     #separation is the ratio of pitch test to gap between tests. values range from 0.0 (no white space) to 1.0 (only white space)
     #margins represents how much white space will be left on the outside of the page in inches. The tuple represents the space from the top, right, bottom, and left, respectively)
-    #header is the space given to the information header in inches
-    #num_width is the width of the segment labels in inches
-    def generate_pitch_sheet(self, directory:str = "./pitch_sheet.png", segments:int = 11, separation:float = 0.2, margins:tuple = (0.25, 0.25, 0.25, 0.25), header_height:float = 1.0, num_width = 0.75):
+    #header is the space given to the information header as a fraction of the available vertical space within the margins
+    #num_width is the width of the segment labels as a fraction of the available horizontal space within the margins
+    def generate_pitch_sheet(self, directory:str = "./pitch_sheet.png", segments:int = 11, separation:float = 0.2, margins:tuple = (0.25, 0.25, 0.25, 0.25), header_height_ratio:float = 0.2, num_width_ratio = 0.1):
         bg = Image.new(mode = "RGB", size = self.page_resolution, color = (255, 255, 255))
+        
+        #header position in inches
+        head_horiz_pos = margins[3]
+        head_vert_pos = margins[0]
+        head_pos = (head_horiz_pos, head_vert_pos)
+        
+        #header position in pixels
+        head_horiz_pos_px = head_pos[0] * self.page_dpi[0]
+        head_vert_pos_px = head_pos[1] * self.page_dpi[1]
+        head_pos_px = (head_horiz_pos_px, head_vert_pos_px)
+        
+        #header dimensions in inches
+        head_horiz_space = self.page_dimensions[0] - margins[1] - margins[3]
+        head_vert_space = header_height_ratio * (self.page_dimensions[1] - (margins[0] + margins[2]))
+        head_space = (head_horiz_space, head_vert_space)
+        
+        #header dimensions in pixels
+        head_horiz_space_px = head_space[0] * self.page_dpi[0]
+        head_vert_space_px = head_space[1] * self.page_dpi[1]
+        head_space_px = (head_horiz_space_px, head_vert_space_px)
+        
+        fnt_path = path.join(path.dirname(path.dirname(__file__)), "fonts/Roboto/Roboto-Regular.ttf")
+        head = self._generate_header_text(fnt_path, 0.1, 0.5, head_space_px)
+        bg.paste(head, (int(head_pos_px[0]), int(head_pos_px[1])))
+        
+        head_height = head_space[1]
+        num_width = num_width_ratio * (self.page_dimensions[0] - (margins[1] + margins[3]))
         available_horiz = self.page_dimensions[0] - margins[1] - margins[3] - num_width
-        available_vert = self.page_dimensions[1] - margins[0] - margins[2] - header_height
+        available_vert = self.page_dimensions[1] - margins[0] - margins[2] - head_space[1]
         available_space = (available_horiz, available_vert)
         
         #loop generates the images for each pitch segment and their according lpi labels
@@ -78,12 +141,12 @@ class PitchSheet:
         for i in range(segments):
             #segment positions in inches
             seg_horiz_pos = margins[3] + num_width
-            seg_vert_pos = margins[0] + header_height + (i * (available_space[1] / segments))
+            seg_vert_pos = margins[0] + head_height + (i * (available_space[1] / segments))
             seg_pos = (seg_horiz_pos, seg_vert_pos)
             
             #label positions in inches
             lab_horiz_pos = margins[3]
-            lab_vert_pos = margins[0] + header_height + (i * (available_space[1] / segments))
+            lab_vert_pos = margins[0] + head_height + (i * (available_space[1] / segments))
             lab_pos = (lab_horiz_pos, lab_vert_pos)
             
             #segment positions in pixels
@@ -123,9 +186,7 @@ class PitchSheet:
             seg = self._generate_pitch_segment(seg_space, seg_space_px, lpi)
             bg.paste(seg, (int(seg_pos_px[0]), int(seg_pos_px[1])))
             
-            fnt_path = path.join(path.dirname(path.dirname(__file__)), "fonts/Roboto/Roboto-Regular.ttf")
-            #lab_fnt = ImageFont.truetype("../fonts/Roboto/Roboto-Regular.ttf", int(lab_space_px[1])) #font for pitch segment labels
-            lab = self._generate_segment_label(fnt_path, 0.35, lab_space, lab_space_px, lpi)
+            lab = self._generate_segment_label(fnt_path, 0.3, lab_space_px, lpi)
             bg.paste(lab, (int(lab_pos_px[0]), int(lab_pos_px[1])))
             
             draw = ImageDraw.Draw(bg)
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from lenticular.pitch_sheet import PitchSheet
 def main():
     low_pitch = 74.0
     high_pitch = 76.0
-    page_dimensions = (8.5, 11.0)
+    page_dimensions = (11.0, 8.5) #(8.5, 11.0)
     page_dpi = (360, 360)
-    ps = PitchSheet(low_pitch, high_pitch, page_dimensions, page_dpi)
+    ps = PitchSheet(low_pitch, high_pitch, page_dimensions, page_dpi, False)
     ps.generate_pitch_sheet()
 
 if __name__ == "__main__":
